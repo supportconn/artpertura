ac9843cf4e1ed389e2c6ddeb282f36e458b76553
chironliu <14323669+chironliu@users.noreply.github.com>
2025-12-23 10:12:05 +0800

    update

--- artpertura1.1/vite.config.ts
+++ artpertura1.1/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/artpertura/artpertura1.1/',
+      base: '/artpertura/artpertura1.1/dist/',
       server: {
         port: 3000,
         host: '0.0.0.0',
